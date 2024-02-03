1dbd1d39db7c0aae54b0412831dd6dfef11e1939
varungobinath <varung31102004@gmail.com>
2024-02-03 22:29:13 +0530

    ft:Day3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,8 @@ public class Main {
     public static void main(String[] args) {
         //Day 1'
         int n1=9,n2=3;
+        //Day3
+        int t = n1+n2;
+        System.out.println(t);
     }
 }
